eb8191858fe793e3b597f4425ee4d0f737bf06e1
Santiago Sanchez-Ramirez <santiago.snchez@gmail.com>
2021-08-04 23:40:05 -0300

    merged branch with master

--- faSomeRecords.py
+++ faSomeRecords.py
@@ -1,88 +1,8 @@
 #!/usr/bin/env python3
 # faSomeRecords.py
 
-import argparse
-import textwrap
+import argparse, textwrap
 import sys
-<<<<<<< HEAD
-
-
-def main():
-    # parse arguments
-    parser = argparse.ArgumentParser(prog="FaSomeRecords.py", formatter_class=argparse.RawTextHelpFormatter,
-                                     description="Retrieves some FASTA records provided"
-                                                 "a FASTA file and a list or records.", epilog=textwrap.dedent('''\
-    Records can be specified providing a list file with --list/-l
-    or one by one in the command line using --records/-r.
-    The elements in the list must be an exact match as those in the
-    master FASTA file, and may or may not include the ">" character at
-    the beginning of the header.
-    Example of list file:
-    sequence1
-    sequence2
-    sequence3
-    ...
-    sequenceN
-    '''))
-    parser.add_argument('--fasta', '-f', metavar='FASTA_FILE', type=str, required=True,
-                        help='FASTA file where all the sequences are stored.')
-    parser.add_argument('--list', '-l', metavar='LIST', type=str, help='file name of the list.')
-    parser.add_argument('--records', '-r', metavar='RECORD', nargs="*", type=str, help='individual FASTA records.')
-    parser.add_argument('--outfile', '-o', default="records.fasta", nargs="?", type=str,
-                        help='name for output file (default: %(default)s)')
-    parser.add_argument('--stdout', '-s', action="store_true", default=False,
-                        help='if sequences should be printed to screen.')
-    parser.add_argument('--keep', '-k', action="store_true", default=False, help="keep the order in the list.")
-    parser.add_argument('--exclude', '-e', action="store_true", default=False,
-                        help="Exclude these sequences from the final output.")
-    args = parser.parse_args()
-
-    if args.list is None and args.records is None:
-        parser.print_usage()
-        sys.exit("FaSomeRecords.py: error: argument --list/-l or --records/-r is required")
-    if args.list is not None and args.records is None:
-        with open(args.list, "r") as l:
-            heads = l.read().splitlines()
-        if ">" not in heads[0]:
-            heads = [">" + h for h in heads]
-    elif args.records is not None and args.list is None:
-        heads = args.records
-        if ">" not in heads[0]:
-            heads = [">" + h for h in heads]
-
-    requested = len(heads)
-    joinheads = "|" + "|".join(heads) + "|"
-
-    found = 0
-    not_found = []
-
-    if args.exclude:
-        if args.keep:
-            print("--exclude will maintain the order of the original FASTA by default")
-        with open(args.outfile, "w") as out:
-            with open(args.fasta, "r") as f:
-                for line in f:
-                    if line.startswith(">") and line.strip("\n") not in heads:
-                        seq = 1
-                        out.write(line)
-                        found += 1
-                    elif line.startswith(">"):
-                        not_found += [line[1:-1]]
-                        seq = 0
-                    else:
-                        if seq == 1:
-                            out.write(line)
-
-    elif args.keep and not args.exclude:
-        store = {}
-        with open(args.fasta, "r") as f:
-            for line in f:
-                if line[0] == ">":
-                    if "|" + line[:-1] + "|" in joinheads:
-                        h = line[:-1]
-                        seq = 1
-                        store[h] = ''
-=======
 import gzip
 from re import match
 from random import choice
@@ -162,44 +82,12 @@ def parse_fasta(handle, stdout, outfile, order, exclude, requested, joinheads, h
                         h = line[1:].rstrip()
                         seq = 1
                         store[h] = ''
->>>>>>> update_exl
                         found += 1
                     else:
                         seq = 0
                 else:
                     if seq == 1:
                         store[h] += line
-<<<<<<< HEAD
-        if found == 0:
-            print("No sequences found")
-        else:
-            if args.stdout:
-                for h in heads:
-                    if store.get(h):
-                        sys.stdout.write(h + "\n" + store[h])
-                    else:
-                        sys.stdout.write(h + "\n" + "#not found\n")
-                        not_found += [h]
-            else:
-                with open(args.outfile, "w") as o:
-                    for h in heads:
-                        if store.get(h):
-                            o.write(h + "\n" + store[h])
-                        else:
-                            sys.stdout.write(h + "\n" + "#not found\n")
-                            not_found += [h]
-                print("Found {} sequence(s)".format(found))
-                if found > requested:
-                    print("Found {} sequence(s) more than requested".format(found - requested))
-                elif requested > found:
-                    print("Could not find {} sequence(s)".format(requested - found))
-                    print("\n".join(not_found))
-                print("Sequences saved to: " + args.outfile)
-    else:
-        if args.stdout:
-            with open(args.fasta, "r") as f:
-                for line in f:
-=======
         # go through header list and print
         if stdout:
             for head in heads:
@@ -231,29 +119,10 @@ def parse_fasta(handle, stdout, outfile, order, exclude, requested, joinheads, h
             else:
                 for line in handle:
                     # checks for header
->>>>>>> update_exl
                     if line[0] == ">":
                         if "|" + line[1:].rstrip() + "|" in joinheads:
                             # start writing to screen if found
                             seq = 1
-<<<<<<< HEAD
-                            sys.stdout.write(line)
-                            found += 1
-                        else:
-                            not_found += [line[:-1]]
-                            seq = 0
-                    else:
-                        if seq == 1:
-                            sys.stdout.write(line)
-            if found == 0:
-                print("No sequences found")
-        else:
-            with open(args.outfile, "w") as o:
-                with open(args.fasta, "r") as f:
-                    for line in f:
-                        if line[0] == ">":
-                            if "|" + line[:-1] + "|" in joinheads:
-=======
                             print(line, end='')
                             found += 1
                         else:
@@ -292,36 +161,16 @@ def parse_fasta(handle, stdout, outfile, order, exclude, requested, joinheads, h
                             total += 1
                             if "|" + line[1:].rstrip() + "|" in joinheads:
                                 # start writing to screen if found
->>>>>>> update_exl
                                 seq = 1
                                 o.write(line)
                                 found += 1
                             else:
-<<<<<<< HEAD
-                                not_found += [line[1:-1]]
-=======
                                 # count missing if no match
                                 # not_found += [line[1:].rstrip()]
->>>>>>> update_exl
                                 seq = 0
                         else:
                             if seq == 1:
                                 o.write(line)
-<<<<<<< HEAD
-            if found == 0:
-                print("No sequences found")
-            else:
-                print("Found {} sequence(s)".format(found))
-                if found > requested:
-                    print("Found {} sequence(s) more than requested".format(found - requested))
-                elif requested > found:
-                    print("Could not find {} sequence(s)".format(requested - found))
-                    print("\n".join(not_found))
-                print("Sequences saved to " + args.outfile)
-
-
-if __name__ == '__main__':
-=======
                 write_to_file(store, outfile, found, requested, heads)
 
 # main function
@@ -395,5 +244,4 @@ def main():
         parse_fasta(f, args.stdout, args.outfile, args.keep_list_order, args.exclude, requested, joinheads, heads)
 
 if __name__ == "__main__":
->>>>>>> update_exl
     main()
